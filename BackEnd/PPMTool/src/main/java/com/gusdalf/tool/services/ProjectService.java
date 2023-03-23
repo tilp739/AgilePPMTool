a35fb9c6a1a97e626ba6992a79f5f6a03f20ab97
Augustin <morvillez.augustin@gmail.com>
2023-03-23 17:45:41 +0100

    BackEnd

---
+++ BackEnd/PPMTool/src/main/java/com/gusdalf/tool/services/ProjectService.java
@@ -0,0 +1,47 @@
+package com.gusdalf.tool.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gusdalf.tool.domain.Project;
+import com.gusdalf.tool.exceptions.ProjectIdException;
+import com.gusdalf.tool.repositories.ProjectRepository;
+
+@Service
+public class ProjectService {
+
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	public Project saveOrUpdateProject(Project project) {
+		 
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}catch(Exception e) {
+			throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() +"already exists");
+		}
+	}
+	
+	public Project findByProjectIdentifier(String projectId) {
+		
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if (project == null) {
+			throw new ProjectIdException("Project ID "+projectId +" does not exists");
+			
+		}
+		return project;
+	}
+	
+	public Iterable<Project> findAllProjects(){
+		return projectRepository.findAll();
+	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if(project == null) {
+			throw new ProjectIdException("Cannot delete Project with ID " + projectId + ". Project ID "+projectId +" does not exists");
+		}
+		projectRepository.delete(project);
+	}
+}
