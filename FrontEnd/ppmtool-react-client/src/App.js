ccf975d26e87018b8103882c6619aeca87b663ad
Augustin <morvillez.augustin@gmail.com>
2023-03-27 09:08:14 +0200

    Front

--- FrontEnd/ppmtool-react-client/src/App.js
+++ FrontEnd/ppmtool-react-client/src/App.js
@@ -1,13 +1,32 @@
 import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+class App extends Component {
+  render() {
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>;
+  }
 }
 
 export default App;
--- FrontEnd/ppmtool-react-client/src/components/Dashboard.js
+++ FrontEnd/ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
-    return <h1>Welcome to the Dashboard</h1>;
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+            </div>
+            <ProjectItem />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
